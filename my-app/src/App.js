883acbe26222ff7aa3ea218341e333870e4734a0
ginza_mac <ginza_mac@ginza-macui-MacBook-Pro.local>
2017-07-24 00:20:11 +0900

    first

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='helloworld'>
+        helloworld
+      </div>
+    );
+  }
+}
+
+export default App;
