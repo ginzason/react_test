4c90653453a5520cf494c04d0aaa41d5590a9796
ginza_mac <ginza_mac@ginza-macui-MacBook-Pro.local>
2017-07-31 18:02:47 +0900

    react

--- my-app/src/Comments.js
+++ my-app/src/Comments.js
@@ -5,7 +5,7 @@ class Comments extends Component {
   render() {
     return(
       <div>
-        {/* put {} around comments */}
+        {"커맨트를 쓸 수 있음 or 기타 변수넘기는 부분"}
         <span>This component has comments</span>
       </div>
     )
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Kanban from './Kanban/Kanban';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+ReactDOM.render(<Kanban />, document.getElementById('root'));
 registerServiceWorker();
