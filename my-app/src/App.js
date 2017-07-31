333ac458e383cd804a8b73360f66d4539ef75422
ginza_mac <ginza_mac@ginza-macui-MacBook-Pro.local>
2017-07-31 13:21:20 +0900

    jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 
+import Conditional from './Conditional';
+import BlankSpace from './BlankSpace';
+import Comments from './Comments';
+import CommentForm from './CommentForm';
+
+// Parent Component
 class App extends Component {
   render() {
     return (
-      <div className='helloworld'>
-        helloworld
+      <div>
+        <h1>JSX Quirks</h1>
+        <h2>Conditionals</h2>
+        <Conditional />
+        <h2>Blank Space</h2>
+        <BlankSpace />
+        <h2>Comments in JSX</h2>
+        <Comments />
+        <h2>React Without JSX</h2>
+        <CommentForm />
+
       </div>
     );
   }
 }
 
-export default App;
+
+render(<App />, document.getElementById('root'));
